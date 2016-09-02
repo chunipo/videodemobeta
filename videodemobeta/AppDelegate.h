6000c93645e97a47717d228b746b96bc3c0fe095
ptah <ptah@ptahdeimac.local>
2016-09-02 16:24:48 -0700

    Initial Commit

---
+++ videodemobeta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  videodemobeta
+//
+//  Created by ptah on 16/9/2.
+//  Copyright © 2016年 Ptah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ videodemobeta/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  videodemobeta
+//
+//  Created by ptah on 16/9/2.
+//  Copyright © 2016年 Ptah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
